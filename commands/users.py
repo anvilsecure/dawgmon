a5c5e167af449790e95418cf499f023dfd87a840
Vincent Berg <gvb@santarago.org>
2017-09-11 01:01:00 +0200

    track passwordless logins for groups and users
    track passwordless logins for groups and users and display
    warnings if they happen. also track if the passwords are not
    stored in the shadow file but in the /etc/passwd or /etc/group file
    respectively. this also means that potentially sensitive data
    can be stored by these commands; maybe in the future we need
    an option to sanitize the hashes away from it?

--- commands/users.py
+++ commands/users.py
@@ -1,5 +1,13 @@
 from . import *
 
+# pw entry set to "x" means password is stored in /etc/shadow or /etc/gshadow
+# file # and a completely empty entry there means possibly passwordless login
+# for either the user or the group in /etc/passwd or /etc/group respectively.
+def is_empty(pw):
+	return not pw or len(pw) == 0
+def is_gshadow(pw):
+	return pw and pw == "x"
+
 class CheckGroupsCommand(Command):
 	name = "check_groups"
 	shell = False
@@ -13,12 +21,13 @@ class CheckGroupsCommand(Command):
 			users = parts[3].split(",")
 			if len(users[0]) == 0:
 				users = []
-			res[parts[0]] = (int(parts[2]), users)
+			res[parts[0]] = (int(parts[2]), users, parts[1])
 		return res
 
 	def compare(prev, cur):
 		anomalies = []
 		groups = merge_keys_to_list(prev, cur)
+		shadow_groups = []
 		for group in groups:
 			if group not in prev:
 				anomalies.append(C("group %s added" % group))
@@ -27,15 +36,31 @@ class CheckGroupsCommand(Command):
 				anomalies.append(C("group %s removed" % group))
 				continue
 			prev_gid, cur_gid = prev[group][0], cur[group][0]
+			prev_pw, cur_pw = prev[group][2], cur[group][2]
+			prev_users, cur_users = prev[group][1], cur[group][1]
 			if prev_gid != cur_gid:
 				anomalies.append(C("gid of group %s changed from %i to %i" % (group, prev_gid, cur_gid)))
-			prev_users, cur_users = prev[group][1], cur[group][1]
+			if is_gshadow(prev_pw) != is_gshadow(cur_pw):
+				from_file = "gshadow" if is_gshadow(prev_pw) else "group"
+				to_file = "gshadow" if is_gshadow(cur_pw) else "group"
+				anomalies.append(C("password for group %s moved from /etc/%s to /etc/%s" % (group, from_file, to_file)))
+			if is_gshadow(cur_pw) or is_empty(cur_pw):
+				shadow_groups.append(group)
+			if is_empty(cur_pw):
+				anomalies.append(W("password for group %s set to empty (might allow group login)" % (group)))
+
 			all_users = list(set(prev_users + cur_users))
 			for user in all_users:
 				if user not in prev_users:
 					anomalies.append(C("user %s added to group %s" % (user, group)))
 				elif user not in cur_users:
 					anomalies.append(C("user %s removed from group %s" % (user, group)))
+		in_group = list(set(cur.keys()) ^ set(shadow_groups))
+		l = len(in_group)
+		if l == 0:
+			anomalies.append(D("all groups have entries for passwords in /etc/gshadow as they should"))
+			return anomalies
+		anomalies.append(W("%i password%s for groups not in /etc/gshadow but /etc/group [%s]" % (l, "s" if l != 1 else "", ",".join(in_group))))
 		return anomalies
 
 class CheckUsersCommand(Command):
@@ -51,15 +76,18 @@ class CheckUsersCommand(Command):
 			login = parts[0]
 			uid, gid = int(parts[2]), int(parts[3])
 			homedir, shell = parts[5].strip(), parts[6].strip()
-			res[login] = (uid, gid, homedir, shell)
+			res[login] = (uid, gid, homedir, shell, parts[1])
 		return res
 
 	def compare(prev, cur):
 		anomalies = []
 		users = merge_keys_to_list(prev, cur)
+		shadow_users = []
 		for user in users:
 			if user not in prev:
 				anomalies.append(C("user %s added" % user))
+				if cur[user][4]:
+					shadow_users.append(user)
 				continue
 			elif user not in cur:
 				anomalies.append(C("user %s removed" % user))
@@ -73,4 +101,21 @@ class CheckUsersCommand(Command):
 				anomalies.append(C("homedir for user %s changed from %s to %s" % (user, p[2], c[2])))
 			if p[3] != c[3]:
 				anomalies.append(C("shell for user %s changed from %s to %s" % (user, p[3], c[3])))
+			prev_pw, cur_pw = p[4], c[4]
+			if is_gshadow(prev_pw) != is_gshadow(cur_pw):
+				from_file = "shadow" if is_gshadow(prev_pw) else "passwd"
+				to_file = "shadow" if is_gshadow(cur_pw) else "passwd"
+				anomalies.append(C("password for user %s moved from /etc/%s to /etc/%s" % (user, from_file, to_file)))
+			if is_gshadow(cur_pw) or is_empty(cur_pw):
+				shadow_users.append(user)
+			if is_empty(cur_pw):
+				anomalies.append(W("password for user %s set to empty (might allow login)" % (user)))
+
+		# for shadow users check we only care about the current file to display warnings
+		in_passwd = list(set(cur.keys()) ^ set(shadow_users))
+		l = len(in_passwd)
+		if l == 0:
+			anomalies.append(D("all users have entries for passwords in /etc/shadow file as they should"))
+			return anomalies
+		anomalies.append(W("%i password%s for users not in /etc/shadow but /etc/passwd [%s]" % (l, "s" if l != 1 else "", ",".join(in_passwd))))
 		return anomalies
