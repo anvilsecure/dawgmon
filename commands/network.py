935cb7238210b7fc09ebb33cfe2880ca2c0b8d14
Vincent Berg <gvb@santarago.org>
2017-09-10 23:11:45 +0200

    output (tcp/tcp6/udp/udp6) and extra debug info

--- commands/network.py
+++ commands/network.py
@@ -14,15 +14,23 @@ class ListListeningTCPUDPPortsCommand(Command):
 			pe = res.setdefault(port, [])
 			pe.append(proto)
 		for port in res:
-			res[port] = tuple(set(res[port]))
+			res[port] = list(set(res[port]))
+			res[port].sort()
 		return res
 
 	def compare(prev, cur):
 		anomalies = []
 		ports = merge_keys_to_list(prev, cur)
 		for port in ports:
+			prev_types = "/".join(prev[port]) if port in prev else None
+			cur_types = "/".join(cur[port]) if port in cur else None
 			if port not in cur:
-				anomalies.append(C("port %i closed" % port))
+				anomalies.append(C("port %i %s closed" % (port, prev_types)))
+				continue
 			elif port not in prev:
-				anomalies.append(C("port %i opened" % port))
+				anomalies.append(C("port %i %s opened" % (port, cur_types)))
+				continue
+			if prev_types != cur_types:
+				anomalies.append(C("port %i is open and changed from %s to %s" % (port, prev_types, cur_types)))
+			anomalies.append(D("port %i %s is listening" % (port, cur_types)))
 		return anomalies
