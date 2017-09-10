5305091e7845ca9056fd3f046eda7cd6f7ad30eb
Vincent Berg <gvb@santarago.org>
2017-09-11 01:18:01 +0200

    detect time traveling machines
    most likely the explanation is a timezone change or another wall clock change
    which might affect the results of the current uptime comparison versus the
    stored baseline so we throw a warning about it at least.

--- commands/uptime.py
+++ commands/uptime.py
@@ -20,7 +20,11 @@ class UptimeCommand(Command):
 		scur = cur.strftime(PARSE_STR)
 		if not prev:
 			return [D("system has been up since %s" % scur)]
-		if cur != prev and cur > prev:
-			return [W("system rebooted since last check (up since: %s)" % scur)]
+		sprev = prev.strftime(PARSE_STR)
+		if cur != prev:
+			if cur > prev:
+				return [W("system rebooted since last check (up since: %s)" % scur)]
+			else:
+				return [W("time traveling detected as uptime went from %s to %s" % (sprev, scur))]
 		else:
 			return [D("system didn't reboot since last check (up since: %s)" % scur)]
