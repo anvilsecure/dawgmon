91aa6bce5f98829ceb3c8c6c8b1477e2e117e3df
Vincent Berg <gvb@santarago.org>
2017-09-11 15:17:09 +0200

    fixed up ipc functionality and output
    introduced more complete output options for the checking of shared memory
    segments, semaphore arrays and message queues.

--- commands/ipc.py
+++ commands/ipc.py
@@ -26,10 +26,12 @@ class ListSharedMemorySegmentsCommand(Command):
 		segments = merge_keys_to_list(prev, cur)
 		for shmid in segments:
 			if shmid not in cur:
-				anomalies.append(C("shared memory segment %i destroyed" % shmid))
+				p = prev[shmid]
+				anomalies.append(C("shared memory segment %i destroyed (key=0x%x, owner=%s, permissions=%s, size=%i)" % (shmid, p[0], p[1], p[2], p[3])))
 				continue
 			elif shmid not in prev:
-				anomalies.append(C("shared memory segment %i created" % shmid))
+				c = cur[shmid]
+				anomalies.append(C("shared memory segment %i created (key=0x%x, owner=%s, permissions=%s, size=%i)" % (shmid, c[0], c[1], c[2], c[3])))
 				continue
 			p, c = prev[shmid], cur[shmid]
 			if p[0] != c[0]:
@@ -40,6 +42,7 @@ class ListSharedMemorySegmentsCommand(Command):
 				anomalies.append(C("permissions for shared memory segment %i changed from %s to %s" % (shmid, p[2], c[2])))
 			if p[3] != c[3]:
 				anomalies.append(C("size of shared memory segment %i changed from %i to %i" % (shmid, p[3], c[3])))
+			anomalies.append(D("shared memory segment %i (key=0x%x, owner=%s, permissions=%s and size=%i) unchanged" % (shmid, p[0], p[1], p[2], p[3])))
 		return anomalies
 
 class ListSemaphoreArraysCommand(Command):
@@ -52,14 +55,26 @@ class ListSemaphoreArraysCommand(Command):
 
 	def compare(prev, cur):
 		anomalies = []
-		queues = merge_keys_to_list(prev, cur)
-		for q in queues:
-			if q not in cur:
-				anomalies.append(C("semaphore %i destroyed" % q))
+		semaphores = merge_keys_to_list(prev, cur)
+		for sem in semaphores:
+			if sem not in cur:
+				p = prev[sem]
+				anomalies.append(C("semaphore array %i destroyed (key=0x%x, owner=%s, permissions=%s, nsems=%i)" % (sem, p[0], p[1], p[2], p[3])))
 				continue
-			elif q not in prev:
-				anomalies.append(C("semaphore %i created" % q))
+			elif sem not in prev:
+				c = cur[sem]
+				anomalies.append(C("semaphore array %i created (key=0x%x, owner=%s, permissions=%s, nsems=%i)" % (sem, c[0], c[1], c[2], c[3])))
 				continue
+			p, c = prev[sem], cur[sem]
+			if p[0] != c[0]:
+				anomalies.append(C("key for semaphore array %i changed from 0x%x to 0x%x" % (sem, p[0], c[0])))
+			if p[1] != c[1]:
+				anomalies.append(C("owner of semaphore array %i changed from %s to %s" % (sem, p[1], c[1])))
+			if p[2] != c[2]:
+				anomalies.append(C("permissions for semaphore array %i changed from %s to %s" % (sem, p[2], c[2])))
+			if p[3] != c[3]:
+				anomalies.append(C("number of semaphores in semaphore array %i changed from %i to %i" % (sem, p[3], c[3])))
+			anomalies.append(D("semaphore array %i (key=0x%x, owner=%s, permissions=%s, nsems=%i) unchanged" % (sem, p[0], p[1], p[2], p[3])))
 		return anomalies
 
 class ListMessageQueuesCommand(Command):
@@ -75,11 +90,23 @@ class ListMessageQueuesCommand(Command):
 		queues = merge_keys_to_list(prev, cur)
 		for q in queues:
 			if q not in cur:
-				anomalies.append(C("message queue %i destroyed" % q))
+				p = prev[q]
+				anomalies.append(C("message queue %i destroyed (key=0x%x, owner=%s, permissions=%s, used-bytes=%i)" % (q, p[0], p[1], p[2], p[3])))
 				continue
 			elif q not in prev:
-				anomalies.append(C("message queue %i created" % q))
+				c = cur[q]
+				anomalies.append(C("message queue %i created (key=0x%x, owner=%s, permissions=%s, used-bytes=%i)" % (q, c[0], c[1], c[2], c[3])))
 				continue
+			p, c = prev[q], cur[q]
+			if p[0] != c[0]:
+				anomalies.append(C("key for message queue %i changed from 0x%x to 0x%x" % (q, p[0], c[0])))
+			if p[1] != c[1]:
+				anomalies.append(C("owner of message queue %i changed from %s to %s" % (q, p[1], c[1])))
+			if p[2] != c[2]:
+				anomalies.append(C("permissions for message queue %i changed from %s to %s" % (q, p[2], c[2])))
+			if p[3] != c[3]:
+				anomalies.append(C("used-bytes of message queue %i changed from %i to %i" % (q, p[3], c[3])))
+			anomalies.append(D("message queue %i (key=0x%x, owner=%s, permissions=%s, used-bytes=%i) unchanged" % (q, p[0], p[1], p[2], p[3])))
 		return anomalies
 
 class ListListeningUNIXSocketsCommand(Command):
