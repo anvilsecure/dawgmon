80b2d90cde180ad2a0d4a5e53c41b89ccd479dc5
Vincent Berg <gvb@santarago.org>
2017-09-11 21:35:35 +0200

    added list commands and list cache functionality

--- local.py
+++ local.py
@@ -1,12 +1,11 @@
 import subprocess
 import commands, utils
 
-def local_run(dirname, commandlist=None):
+def local_run(dirname, commandlist):
 	res = {}
 	output = utils.get_output_filename(dirname)
-	for cmd in commands.COMMANDS:
-		if commandlist and cmd.name not in commandlist:
-			continue	
+	for cmdname in commandlist:
+		cmd = commands.COMMAND_CACHE[cmdname]
 		subprocess.call("%s > %s" % (cmd.command, output), shell=True)
 		with open(output, "r") as fd:
 			res[cmd.name] = fd.read()
